7756a41acc806c323cf6bfc6cc3dfca9bc66ab28
Fahd Munir <fahd.munir.89@gmail.com>
2022-11-29 17:42:55 -0500

    added crud routes and mongo local version

--- server.js
+++ server.js
@@ -1,13 +1,75 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.get("/", (req, res) => { 
-    res.send("Express is here");
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+mongoose
+  .connect("mongodb://localhost:27017/myDB")
+  .catch((err) => console.log(err));
+
+//DB Schema and Model
+
+const postSchema = mongoose.Schema({
+  title: String,
+  description: String,
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+app.get("/", (req, res) => {
+  res.send("Express is here");
+});
+
+app.post("/create", (req, res) => {
+  Post.create({
+    title: req.body.title,
+    description: req.body.description,
+  })
+    .then((doc) => console.log(doc))
+    .catch((err) => console.log(err));
+});
+
+//  NOTE: for local Mongo to show this in format {
+//         "_id" : ObjectId("638679736d213b15febe6e9b"),
+//         "title" : "Test title",
+//         "description" : "Test description",
+//         "__v" : 0
+//        }
+// Follow commands/steps in bash: 1)mongo 2)show dbs 3)use myDB 4)show collections 5)db.posts.find().pretty()
+
+app.get("/posts", (req, res) => {
+  Post.find()
+    .then((items) => res.json(items))
+    .catch((err) => console.log(err));
+});
+
+app.delete("/delete/:id", (req, res) => {
+  //console.log(req.params); would give { id: abc }
+  Post.findByIdAndDelete({ _id: req.params.id })
+    .then((doc) => console.log(doc))
+    .catch((err) => console.log(err));
+});
+
+app.put("/update/:id", (req, res) => {
+  //console.log(req.params) gives { id: 'abc' }
+  //console.log(req.body) gives { _id: 'abc', title: 'new post title', description:'new description', __v:0}
+
+  Post.findByIdAndUpdate(
+    { _id: req.params.id },
+    {
+      title: req.body.title,
+      description: req.body.description,
+    }
+  )
+    .then((doc) => console.log(doc))
+    .catch((err) => console.log(err));
 });
 
 app.listen(3001, function () {
-    console.log("Server is running at Port 3001");
+  console.log("Server is running at Port 3001");
 });
